8f4b504b3688c0e1baf2c588d60dc1513f4cd0c5
wilschok <william.schokkele@howest.be>
2018-11-25 11:54:18 +0100

    students

---
+++ CoreWebAPI/Data/StudentRepository.cs
@@ -0,0 +1,47 @@
+﻿using CoreWebAPI.Models;
+using DataHelpers.Lib;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CoreWebAPI.Data
+{
+    public class StudentRepository : GenericDataHelper<Student>
+    {
+        string sqlSelect = @"select 
+                                studnr,
+	                            voornaam,
+	                            familienaam,
+	                            (select count(*) from studenten_cursussen sc where studnr = s.studnr) as aantalinschrijvingen
+                                from studenten s";
+        string sqlOrder = " order by voornaam, familienaam";
+
+        public StudentRepository(SqlHelper sqlhelper) : base(sqlhelper)
+        {
+        }
+
+        public override Student ConvertToEntity(DataRow row)
+        {
+            Student studentInschrijving = new Student
+            {
+                Studentnr = Convert.ToInt32(row["studnr"]),
+                Voornaam = row["voornaam"].ToString(),
+                Familienaam = row["familienaam"].ToString(),
+                AantalInschrijvingen = Convert.ToInt32(row["aantalinschrijvingen"])
+            };
+            return studentInschrijving;
+        }
+
+        public List<Student> Get()
+        {
+            return ConvertToList(sqlSelect + sqlOrder);
+        }
+
+        public Student GetById(int id)
+        {
+            SqlParameter param = new SqlParameter("@studnr", id);
+            return ConvertToEntity(sqlSelect +" where studnr = @studnr ", param);
+        }
+    }
+}
